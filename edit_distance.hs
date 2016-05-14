baad4dd8b25edb4e5331217a61ddf3561167c57c
Petr Houška <houskape@gmail.com>
2016-05-15 00:16:03 +0200

    Added subArrayToList function.

--- edit_distance.hs
+++ edit_distance.hs
@@ -1,6 +1,9 @@
 module EditDistance where
    import Data.Array
    
+   subArrayToList :: (Ix a) => Array a b -> (a, a) -> [b]
+   subArrayToList ar bnds = [ar ! i | i <- range bnds]
+   
    mkArray :: (Ix a) => (a -> b) -> (a,a) -> Array a b
    mkArray f bnds = array bnds [(i, f i) | i <- range bnds]
    
