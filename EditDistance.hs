1a8b7f248e0bd98cfdd2b0fe26838222f79bc3ff
Petr Houška <houskape@gmail.com>
2016-05-15 20:01:08 +0200

    Implemented initial support for arbitrary delete functions.

--- EditDistance.hs
+++ EditDistance.hs
@@ -12,21 +12,35 @@ module EditDistance where
       ar_bounds   = ((0, 0), (xl, yl))
          
       table = mkArray dist ar_bounds    
+      frem = FRem 5 (\x -> Regular 1)
       
       dist::(Int, Int) -> Infinitable Int
       dist (i, j) 
-         | i > xl || j > yl = PositiveInfinity
-         | i == xl          = Regular (yl - j)
-         | j == yl          = Regular (xl - i)
-         | otherwise = minimum [
-            (table ! (i, j + 1)) + 1,
-            (table ! (i + 1, j)) + 1,
-            if (x ! i) == (y ! j) then table ! (i + 1, j + 1)
-              else (table ! (i + 1, j + 1)) + 1
-            ]
+         | i > xl || j > yl   = PositiveInfinity
+         | i == xl && j == yl = Regular 0
+         | i == xl            = Regular (yl - j)
+         | otherwise = minimum ([
+            if j /= yl 
+              then (table ! (i, j + 1)) + 1
+              else PositiveInfinity,
+            
+            if j /= yl 
+              then if (x ! i) == (y ! j)  
+                then table ! (i + 1, j + 1)
+                else (table ! (i + 1, j + 1)) + 1
+              else PositiveInfinity
+            ] ++ remOptions table frem x (i, j))
                
+
+   data FRem a = FRem Int ([a] -> Infinitable Int)
+   data FAdd a = FAdd Int ([a] -> Infinitable Int)
+   data FMod a = FMod Int ([a] -> [a] -> Infinitable Int)      
    
-   data FRem a b = FRem Int ([a] -> b)
-   data FAdd a b = FAdd Int ([a] -> b)
-   data FMod a b = FMod Int ([a] -> [a] -> b)      
+
+   remOptions :: Array (Int, Int) (Infinitable Int) -> FRem a -> Array Int a -> (Int, Int) -> [Infinitable Int]
+   remOptions ar (FRem maxlf frem) xs (i, j) = [ (ar ! (i + x + 1, j)) + frem (take x maxSubstring) | x <- [0..maxIDeleted]]
+     where 
+       lastDeleted = minimum [length xs - 1, i + maxlf - 1]
+       maxIDeleted = lastDeleted - i;
+       maxSubstring = subArrayToList xs (i, lastDeleted)
   
