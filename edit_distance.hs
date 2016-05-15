11ac36584eee091302f060d39c347e1339a4ab6b
Petr Houška <houskape@gmail.com>
2016-05-15 02:07:09 +0200

    Added support for infinity.

--- edit_distance.hs
+++ edit_distance.hs
@@ -6,8 +6,46 @@ module EditDistance where
    
    mkArray :: (Ix a) => (a -> b) -> (a,a) -> Array a b
    mkArray f bnds = array bnds [(i, f i) | i <- range bnds]
+     
+   data Infinitable a = Regular a | NegativeInfinity | PositiveInfinity deriving (Eq, Show)
+
+   instance Ord a => Ord (Infinitable a) where
+      compare NegativeInfinity NegativeInfinity = EQ
+      compare PositiveInfinity PositiveInfinity = EQ
+      compare NegativeInfinity _ = LT
+      compare PositiveInfinity _ = GT
+      compare _ PositiveInfinity = LT
+      compare _ NegativeInfinity = GT
+      compare (Regular x) (Regular y) = compare x y    
    
-   editDistance :: Eq a => [a] -> [a] -> Int
+   instance Num a => Num (Infinitable a) where
+      negate NegativeInfinity = PositiveInfinity
+      negate PositiveInfinity = NegativeInfinity
+      negate (Regular a) = Regular (negate a)
+      
+      (+) PositiveInfinity _ = PositiveInfinity
+      (+) _ PositiveInfinity = PositiveInfinity
+      (+) NegativeInfinity _ = NegativeInfinity
+      (+) _ NegativeInfinity = NegativeInfinity
+      (+) (Regular a) (Regular b) = Regular (a + b)
+      
+      (*) PositiveInfinity _ = error "Unimplemented"
+      (*) _ PositiveInfinity = error "Unimplemented"
+      (*) NegativeInfinity _ = error "Unimplemented"
+      (*) _ NegativeInfinity = error "Unimplemented"
+      (*) (Regular a) (Regular b) = Regular (a * b)
+      
+      fromInteger a = Regular (fromInteger a)
+      
+      abs NegativeInfinity = PositiveInfinity
+      abs PositiveInfinity = PositiveInfinity
+      abs (Regular a) = Regular (abs a)
+      
+      signum NegativeInfinity = -1
+      signum PositiveInfinity = 1
+      signum (Regular a) = Regular (signum a)
+   
+   editDistance :: Eq a => [a] -> [a] -> Infinitable Int
    editDistance xs ys = table ! (0, 0) where
       x        = array (0, xl) (zip [0..] xs)
       y        = array (0, yl) (zip [0..] ys)
@@ -17,15 +55,15 @@ module EditDistance where
          
       table = mkArray dist ar_bounds    
       
-      dist::(Int, Int) -> Int
+      dist::(Int, Int) -> Infinitable Int
       dist (i, j) 
-         | i == xl = yl - j
-         | j == yl = xl - i
+         | i == xl = Regular (yl - j)
+         | j == yl = Regular (xl - i)
          | otherwise = minimum [
             (table ! (i, j + 1)) + 1,
             (table ! (i + 1, j)) + 1,
             if (x ! i) == (y ! j) then table ! (i + 1, j + 1)
-              else table ! (i + 1, j + 1) + 1
+              else (table ! (i + 1, j + 1)) + 1
             ]   
        
 
