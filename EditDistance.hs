f050a908aeabb893bed31f1de58e7111d158a02d
Petr Houška <houskape@gmail.com>
2016-05-15 02:42:45 +0200

    Moved helper methods to a separate file.

--- EditDistance.hs
+++ EditDistance.hs
@@ -1,12 +1,7 @@
 module EditDistance where
    import Data.Array
    import InfinitySupport
-   
-   subArrayToList :: (Ix a) => Array a b -> (a, a) -> [b]
-   subArrayToList ar bnds = [ar ! i | i <- range bnds]
-   
-   mkArray :: (Ix a) => (a -> b) -> (a,a) -> Array a b
-   mkArray f bnds = array bnds [(i, f i) | i <- range bnds]
+   import OtherHelpers
    
    editDistance :: Eq a => [a] -> [a] -> Infinitable Int
    editDistance xs ys = table ! (0, 0) where
---
+++ OtherHelpers.hs
@@ -0,0 +1,8 @@
+module OtherHelpers where     
+   import Data.Array
+   
+   subArrayToList :: (Ix a) => Array a b -> (a, a) -> [b]
+   subArrayToList ar bnds = [ar ! i | i <- range bnds]
+   
+   mkArray :: (Ix a) => (a -> b) -> (a,a) -> Array a b
+   mkArray f bnds = array bnds [(i, f i) | i <- range bnds]
