3b00e84e35e1a9bc155b7411e414663ba065bcb1
Petr Houška <houskape@gmail.com>
2016-05-16 16:44:35 +0200

    Code cleanup in main file.

--- EditDistance.hs
+++ EditDistance.hs
@@ -3,11 +3,17 @@ module EditDistance where
    import InfinitySupport
    import OtherHelpers
    
+   --Helper type aliases
    type DistScore = (Infinitable Int)
    type MemtableIndex = (Int, Int)
    type Memtable = Array MemtableIndex DistScore
-
-    
+   
+   --Data types used for definition of custom remove, add, and modify scoring functions 
+   data FUn a = FUn Int ([a] -> DistScore) 
+   data FBin a = FBin (Int, Int) ([a] -> [a] -> DistScore)   
+   data UnOp = Del | Add deriving (Eq)
+   
+   --Default edit distance function that uses regular scoring functions 
    editDistanceDef :: Eq a => [a] -> [a] -> Maybe Int
    editDistanceDef = editDistance frem fadd fbin
     where 
@@ -15,21 +21,25 @@ module EditDistance where
       fadd = FUn 1 (\x -> Regular 1)
       fbin = FBin (1,1) (\x y -> if x == y then Regular 0 else Regular 1)
    
+   --Generic edit distance function that accepts definition of custom remove, add, and modify scoring functions
    editDistance :: Eq a => FUn a -> FUn a -> FBin a -> [a] -> [a] -> Maybe Int
    editDistance frem fadd fbin xs ys = infinitableToMaybe $ table ! (0, 0) where
-      x = listToArray xs
+      
+      --Transforms string list into an array for performance reasons
+      x = listToArray xs 
       y = listToArray ys
        
+      --Sets up bounds for memoization table and creates it
       (xl, yl) = (length xs, length ys)      
       ar_bounds   = ((0, 0), (xl, yl))
-         
       table = mkArray dist ar_bounds
       
+      --Defines distance function
       dist::MemtableIndex->DistScore
       dist (i, j) 
-         | i > xl || j > yl   = PositiveInfinity
-         | i == xl && j == yl = Regular 0
-         | otherwise = minimum (
+         | i > xl || j > yl   = PositiveInfinity --If we're out of bounds then the length is PositiveInfinity
+         | i == xl && j == yl = Regular 0        --If we're at the right down corner then we're done and result is zero
+         | otherwise = minimum (  --Otherwise let's take a minimum out of all the options for deletion, removal, and modification starting at current index
               delOptions (i, j) 
               ++ addOptions (i, j)
               ++ modOptions (i, j)
@@ -37,13 +47,9 @@ module EditDistance where
           where 
             delOptions = unModOptions table Del frem x
             addOptions = unModOptions table Add fadd y
-            modOptions = binModOptions table fbin x y 
-               
-
-   data FUn a = FUn Int ([a] -> DistScore)
-   data FBin a = FBin (Int, Int) ([a] -> [a] -> DistScore)   
-   data UnOp = Del | Add deriving (Eq)
+            modOptions = binModOptions table fbin x y               
    
+   --Creates a list of all possibilities for unary modification function (removal, addition) starting at current index 
    unModOptions :: Memtable -> UnOp -> FUn a -> Array Int a -> MemtableIndex -> [DistScore]
    unModOptions ar dir (FUn maxlf fmod) str (i, j) = [ (ar ! getIndex (i, j) x) + fmod (take x maxSubstring) | x <- [1..maxModified]]
      where 
@@ -55,7 +61,7 @@ module EditDistance where
        getIndex (i, j) x | dir == Del = (i + x, j)
                          | otherwise  = (i, j + x)
                          
-                                         
+   --Creates a list of all possibilities for binary modification function (modification) starting at current index                                       
    binModOptions :: Memtable -> FBin a -> Array Int a -> Array Int a -> MemtableIndex -> [DistScore]
    binModOptions ar (FBin (maxlxf, maxlyf) fmod) strX strY (i, j) = [ (ar ! (i + x, j + y)) + funcMod x y | x <- [1..maxModifiedX], y <- [1..maxModifiedY]]
      where 
